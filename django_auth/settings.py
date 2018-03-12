3005becac7c120d34807726ffd70868989036802
Jordan Sutton <jordan.a.sutton@gmail.com>
2018-03-12 09:36:49 +0000

    messenger app

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-messaging-app-with-auth-kgmaxwell1990.c9users.io']
+ALLOWED_HOSTS = ['jord-messenger-jordisutts.c9users.io']
 
 
 # Application definition
